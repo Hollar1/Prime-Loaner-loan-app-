35c63aa4eb9692d89108b5e6d07992c090646709
hollarthunde <abdulkareemumar1989@gmail.com>
2025-09-14 08:44:29 +0100

    updated

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -3,6 +3,7 @@ import styles from "../profile/profile.module.scss";
 import male_icon from "../../assets/images/male_icon.webp";
 import female_icon from "../../assets/images/female_icon.webp";
 import { Line, Circle } from "rc-progress";
+import back_icon from "../../assets/icons/back_icon.png";
 import {
   MdOutlineMail,
   MdOutlinePhone,
@@ -14,14 +15,16 @@ import {
 function Profile() {
   return (
     <div className={styles.parent_wrapper}>
-      <NavBar />
+      <NavBar goBack={back_icon} pageHeader={"Profile"} />
       <div className={styles.wrapper}>
         <section className={styles.sec_01}>
           <img src={male_icon} alt="" />
-          <strong>Umar Kareem</strong>
-          <p>Bike Loan</p>
-          <span>Joined: april 2025</span>
-          <button>Edit Profile</button>
+          <div>
+            <strong>Umar Kareem</strong>
+            <p>Bike Loan</p>
+            <span>Joined: april 2025</span>
+          </div>
+          {/* <button>Edit Profile</button> */}
         </section>
 
         <section className={styles.sec_02}>
@@ -61,29 +64,28 @@ function Profile() {
           <h3>Current Loan Status</h3>
 
           <article>
-            <div>
-              <Circle
-                percent={70}
-                strokeWidth={4}
-                strokeColor="#00ff00"
-                trailWidth={4}
-              />
+            <div className={styles.payment_div}>
+              <div className={styles.totalLoan}>
+                <p>Total Loan:</p>
+                <strong>$5,000</strong>
+              </div>
 
-              <aside>
-                <div>
-                  <p>Total Loan</p>
-                  <b>$5,000</b>
-                </div>
-                <hr />
+              <div className={styles.progressBar}>
+                <Line
+                  percent={70}
+                  strokeWidth={4}
+                  strokeColor="#00ff00"
+                  trailWidth={4}
+                />
+              </div>
 
-                <div>
-                  <p> Balance</p>
-                  <b>$1,200</b>
-                </div>
-              </aside>
+              <div className={styles.RemainingBalance}>
+                <p>Balance:</p>
+                <strong>$1,200</strong>
+              </div>
             </div>
 
-            <div>
+            <div className={styles.payment_date_div}>
               <span>Next payment Due</span>
               <p>June 15 2025</p>
               <button>Pay Now</button>
@@ -93,7 +95,6 @@ function Profile() {
 
         <section className={styles.sec_04}>
           <h3>Payment History</h3>
-
           <article>
             <div>
               <MdCalendarMonth />
@@ -103,8 +104,36 @@ function Profile() {
               <MdKeyboardArrowDown />
             </span>
           </article>
-          <hr />
-          hhh
+
+     
+
+          <main>
+            <aside>
+              <strong>$240.00</strong>
+              <p>March 15, 2025</p>
+            </aside>
+            <span>5 of 6 months</span>
+          </main>
+
+          <main>
+            <aside>
+              <strong>$240.00</strong>
+              <p>February 15, 2025</p>
+            </aside>
+            <span>4 of 6 months</span>
+          </main>
+
+          <main>
+            <aside>
+              <strong>$240.00</strong>
+              <p>January 15, 2025</p>
+            </aside>
+            <span>3 of 6 months</span>
+          </main>
+
+
+
+
         </section>
       </div>
     </div>
